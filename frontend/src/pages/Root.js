ef581ffa8102aac2bcaa14d62e22565923efdc22
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-04-06 19:47:27 +0530

    loader() [ usage, where to execute, Navigation for loading state ]

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,12 +1,15 @@
 import React from "react";
 import MainNavigation from "../components/MainNavigation";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 export default function RootLayout() {
+  // const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
