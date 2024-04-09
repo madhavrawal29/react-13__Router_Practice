51253da1389202b26bf8d4e8b130b0c3cd64ece0
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-04-09 17:44:22 +0530

    Added basic home page and reduced fetching events time

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res, next) => {
     const events = await getAll();
     setTimeout(() => {
       res.json({ events: events });
-    }, 2000);
+    }, 1500);
   } catch (error) {
     next(error);
   }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PageContent from "../components/PageContent.js";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
 }
